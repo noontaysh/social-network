5c0c9be50a39570a8df1fb217f6c87811c4f87f3
Johnny <n00ntaysh@gmail.com>
2023-04-09 16:09:59 +0300

    feat: create header navigation
    Indeed, now there is navigation, but it is hard-coded. For example, by
    clicking on a profile, you will constantly go to 11 identifiers. Or, for
    remote parameters, there are no components at all. However, it was created
    for the convenience of working with the application.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import './App.scss'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import React from "react";
 import Profile from "./features/profile/Profile.jsx";
+import Header from "./components/header/Header.jsx";
+import './common/Container.scss'
 
 function App() {
     return (
-        <div className="App">
+        <div className="App container">
             <BrowserRouter>
+                <Header />
                 <Routes>
                     <Route path={'/profile/:userId'} element={
                         <Profile/>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import './Header.scss'
+
+const Header = () => {
+    return (
+        <header>
+            <img src="https://cryptologos.cc/logos/aave-aave-logo.png" alt=""/>
+            <div className={'header__content'}>
+                <NavLink to={'/profile/11'} className={(navData) => (navData.isActive && 'link__active')}>
+                    <p>Profile</p>
+                </NavLink>
+                <NavLink to={'/users'} className={(navData) => (navData.isActive && 'link__active')}>
+                    <p>Users</p>
+                </NavLink>
+                <NavLink to={'/dialogs'} className={(navData) => (navData.isActive && 'link__active')}>
+                    <p>Messages</p>
+                </NavLink>
+                <NavLink to={'/news'} className={(navData) => (navData.isActive && 'link__active')}>
+                    <p>News</p>
+                </NavLink>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
