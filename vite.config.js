bb525d46a935275ba3d5dfb4524a9228dad7c694
Johnny <n00ntaysh@gmail.com>
2023-06-08 21:31:05 +0300

    feat: setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/way-of-the-samurai",
   plugins: [react()],
   server: {
     port: 3000
